cbf8b63e9fff551f808dfe7eab70c252a401c9fe
timo <44401485+Timo972@users.noreply.github.com>
2025-12-17 10:58:23 +0100

    fix: make app starter workflow compatible with latest portal

--- droidrun/tools/__init__.py
+++ droidrun/tools/__init__.py
@@ -3,7 +3,9 @@ DroidRun Tools - Core functionality for Android device control.
 """
 
 from droidrun.tools.adb import AdbTools
+from droidrun.tools.cloud import MobileRunTools
 from droidrun.tools.ios import IOSTools
+from droidrun.tools.stealth_adb import StealthAdbTools
 from droidrun.tools.tools import Tools, describe_tools
 
 __all__ = [
@@ -12,4 +14,5 @@ __all__ = [
     "AdbTools",
     "IOSTools",
     "StealthAdbTools",
+    "MobileRunTools",
 ]
